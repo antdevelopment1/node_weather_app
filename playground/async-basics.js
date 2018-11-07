fb7befa2cd1b1aa487be8ad3f7f319f6fe183c5e
April Copes <antdevelopment1@gmail.com>
2018-11-06 20:54:44 -0500

    Basic async example

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -1,17 +1,17 @@
 // Step 1
-console.log('Starting app');
+console.log('Starting app 1');
 
 // Step 4
 setTimeout( () => {
-    console.log('Inside of callback');
+    console.log('Inside of callback 4');
 }, 2000);
 
 // Step 3
 setTimeout(() => {
-    console.log('Howdy I am before the other set timeout even though I came after');
+    console.log('Howdy I am before the other set timeout even though I came after 3');
 }, 0)
 
 // Step 2
-console.log('Finishing app');
+console.log('Finishing app 2');
 
 
