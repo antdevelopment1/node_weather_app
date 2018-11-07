4509b8326f04c5ca9a0e0e0de3a97381c72af933
April Copes <antdevelopment1@gmail.com>
2018-11-06 19:35:10 -0500

    Adds set timeout with a 0 timer to illustrate details about the event queue

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -1,11 +1,12 @@
 // Step 1
 console.log('Starting app');
 
-// Step 3
+// Step 4
 setTimeout( () => {
     console.log('Inside of callback');
 }, 2000);
 
+// Step 3
 setTimeout(() => {
     console.log('Howdy I am before the other set timeout even though I came after');
 }, 0)
