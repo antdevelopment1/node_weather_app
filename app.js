47afc640ff071ca8e1c6fbc1ca1c77f04613b5fd
April Copes <antdevelopment1@gmail.com>
2018-11-09 20:31:43 -0500

    Refactors code to call geocodeAddress function with error handeling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs.options({
     a: {
@@ -13,23 +14,18 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv;
 
-let encodedAddress = encodeURIComponent(argv.a);
 
-request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=yBhtlHjM1bPrsZdpmAvGlqoG7mrW4Vyw&location=${encodedAddress}&fbclid=IwAR3KoGjr_3fe4JB0XJnj9BdVztH5Envd05EQVxvwQcQLtdkYcjo0t2g55Yo`,
-    json: true
-}, (error, response, body) => {
-        console.log(body.results[0].locations[0].geocodeQualityCode);
-    if (error) {
-        console.log('Unable to connect to google servers.');
-    } else if (body.results[0].locations[0].geocodeQualityCode !== 'P1AAA') {
-        console.log('Unable to find that address');
-    } else if (body.results[0].locations[0].geocodeQualityCode === 'P1AAA') {
-        console.log(`Address: ${JSON.stringify(body.results[0].providedLocation.location)}`);
-        console.log(`Longitude: ${JSON.stringify(body.results[0].locations[0].latLng.lng)}`);
-        console.log(`Laditude: ${JSON.stringify(body.results[0].locations[0].latLng.lat)}`);  
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log('Unable to find that address.');
+    } {
+        console.log(JSON.stringify(results, undefined));
     }
+});
+
+
+
+
+
     
     
-    // console.log(JSON.stringify(response, undefined, 2));
-})
