21b6c137b7c7a4ec326c18eb8ee0c3c072fa1e0d
April Copes <antdevelopment1@gmail.com>
2018-11-10 12:24:57 -0500

    Refactors weather api call as the next chain in our mapquest api function call

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-// const yargs = require('yargs');
+const yargs = require('yargs');
 
-// const geocode = require('./geocode/geocode');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-// const argv = yargs.options({
-//     a: {
-//         demand: true,
-//         alias: 'address',
-//         describe: 'Address to fetch weather for',
-//         string: true
-//     }
-// })
-// .help()
-// .alias('help', 'h')
-// .argv;
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
 
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log('Unable to find that address.');
-//     } {
-//         console.log(JSON.stringify(results, undefined));
-//     }
-// });
-
-const request = require('request');
-
-request({
-    url: `https://api.darksky.net/forecast/c9213cc8e4c30524bdeafc591714875c/42.3601,-71.0589`,
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(`The temperature currently is: ${body.currently.temperature} degrees F.`) 
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('Unable to fetch weather');
+        console.log(results.address);
+        weather.getWeather(results.Longitude, results.Latitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`{It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}}`);
+            }
+        });
     }
 });
+
+// lat,long, callback
+
 
 
---
+++ weather/weather.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+
+let getWeather = (lat, lng, callback) => {
+
+    request({
+        url: `https://api.darksky.net/forecast/c9213cc8e4c30524bdeafc591714875c/${lng},${lat}`,
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature
+            }); 
+        } else {
+            callback('Unable to fetch weather.');
+        }
+    });
+    
+}
+
+module.exports.getWeather = getWeather;
